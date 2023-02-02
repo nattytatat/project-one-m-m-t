6426c35ad23fdaacb97225f86266ea9ede71cb7c
Natalie <natalie@bigorangemedia.co.uk>
2023-02-02 18:27:58 +0000

    added API file and some functions to script

---
+++ assets/examplereturn.js
@@ -0,0 +1,204 @@
+var result = {
+    "results": [
+    {
+    "address_components": [
+    {
+    "long_name": "Kent",
+    "short_name": "Kent",
+    "types": [
+    "locality",
+    "political"
+    ]
+    },
+    {
+    "long_name": "Portage County",
+    "short_name": "Portage County",
+    "types": [
+    "administrative_area_level_2",
+    "political"
+    ]
+    },
+    {
+    "long_name": "Ohio",
+    "short_name": "OH",
+    "types": [
+    "administrative_area_level_1",
+    "political"
+    ]
+    },
+    {
+    "long_name": "United States",
+    "short_name": "US",
+    "types": [
+    "country",
+    "political"
+    ]
+    }
+    ],
+    "formatted_address": "Kent, OH, USA",
+    "geometry": {
+    "bounds": {
+    "northeast": {
+    "lat": 41.1756921,
+    "lng": -81.3099939
+    },
+    "southwest": {
+    "lat": 41.1176459,
+    "lng": -81.392454
+    }
+    },
+    "location": {
+    "lat": 41.1536674,
+    "lng": -81.3578859
+    },
+    "location_type": "APPROXIMATE",
+    "viewport": {
+    "northeast": {
+    "lat": 41.1756921,
+    "lng": -81.3099939
+    },
+    "southwest": {
+    "lat": 41.1176459,
+    "lng": -81.392454
+    }
+    }
+    },
+    "place_id": "ChIJibNT-BclMYgRj8SEQpjpMso",
+    "types": [
+    "locality",
+    "political"
+    ]
+    },
+    {
+    "address_components": [
+    {
+    "long_name": "Kent",
+    "short_name": "Kent",
+    "types": [
+    "locality",
+    "political"
+    ]
+    },
+    {
+    "long_name": "King County",
+    "short_name": "King County",
+    "types": [
+    "administrative_area_level_2",
+    "political"
+    ]
+    },
+    {
+    "long_name": "Washington",
+    "short_name": "WA",
+    "types": [
+    "administrative_area_level_1",
+    "political"
+    ]
+    },
+    {
+    "long_name": "United States",
+    "short_name": "US",
+    "types": [
+    "country",
+    "political"
+    ]
+    }
+    ],
+    "formatted_address": "Kent, WA, USA",
+    "geometry": {
+    "bounds": {
+    "northeast": {
+    "lat": 47.4411469,
+    "lng": -121.9949869
+    },
+    "southwest": {
+    "lat": 47.3298462,
+    "lng": -122.3096626
+    }
+    },
+    "location": {
+    "lat": 47.3830671,
+    "lng": -122.234771
+    },
+    "location_type": "APPROXIMATE",
+    "viewport": {
+    "northeast": {
+    "lat": 47.4411469,
+    "lng": -121.9949869
+    },
+    "southwest": {
+    "lat": 47.3298462,
+    "lng": -122.3096626
+    }
+    }
+    },
+    "place_id": "ChIJDWzOLRJYkFQRWgAVv23IR10",
+    "types": [
+    "locality",
+    "political"
+    ]
+    },
+    {
+    "address_components": [
+    {
+    "long_name": "Kent",
+    "short_name": "Kent",
+    "types": [
+    "administrative_area_level_2",
+    "political"
+    ]
+    },
+    {
+    "long_name": "England",
+    "short_name": "England",
+    "types": [
+    "administrative_area_level_1",
+    "political"
+    ]
+    },
+    {
+    "long_name": "United Kingdom",
+    "short_name": "GB",
+    "types": [
+    "country",
+    "political"
+    ]
+    }
+    ],
+    "formatted_address": "Kent, UK",
+    "geometry": {
+    "bounds": {
+    "northeast": {
+    "lat": 51.48227240000001,
+    "lng": 1.4517685
+    },
+    "southwest": {
+    "lat": 50.9105199,
+    "lng": 0.0335197
+    }
+    },
+    "location": {
+    "lat": 51.2787075,
+    "lng": 0.5217254000000001
+    },
+    "location_type": "APPROXIMATE",
+    "viewport": {
+    "northeast": {
+    "lat": 51.48227240000001,
+    "lng": 1.4517685
+    },
+    "southwest": {
+    "lat": 50.9105199,
+    "lng": 0.0335197
+    }
+    }
+    },
+    "place_id": "ChIJl4Kx4hWo2EcRWRUgdmTnUZI",
+    "types": [
+    "administrative_area_level_2",
+    "political"
+    ]
+    }
+    ],
+    "status": "OK"
+    }
--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,57 @@ $.ajax({
 });
 
 
+// latitute and longitude variable declared outside, so they can change on page reload or when the findLocation is called
+
+var lat = 51.507351;
+var lon = -0.127758;
+
+// map function
+function showMap() {
+    // identify where the map will be on doc
+    var mapElement = $('#map');
+    //create an instance of the map, passing the variable as a parameter, and then customisation objects
+    var theMap = new google.maps.Map(mapElement[0], {
+        zoom: 15,
+        center: {lat: lat, lng: lon},
+    })
+}
+
+// call Google API for location search and map display
+// pass the argument in the function
+function findLocation(userLocation) {
+    var mapElement = $('#map');
+    //geocode API url with address parameter
+    var baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+    var mapURL = baseURL + userLocation + '&key=' + mapKey
+    console.log(userLocation);
+
+    $.ajax({
+        url: mapURL,
+        method: 'GET'
+    }).then(function(result){
+        // reassign the new lat and lon values to pass through the new map and weather query
+        lat = result.results[0].geometry.location.lat;
+        lon = result.results[0].geometry.location.lon;
+
+        console.log('lat: ' + lat + 'lon: ' + lon);
+        // change the map to new location
+        // theMap = new google.maps.Map(mapElement[0], {
+        //     zoom: 15,
+        //     center: {lat: lat, lng: lon},
+        // })
+        console.log(mapURL);
+    })
+}
+
+$('#location-search').on('click', function (event){
+    event.preventDefault();
+    // pass the user Location on event listener
+    var userLocation = $('#user-location').val();
+
+    // upon click, run the function - pass the variable as an argument
+    findLocation(userLocation);
+})
 
 
 
